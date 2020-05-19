6e91af6a7e95634b9b238dde1790e4584a79338b
ZAHID-8017 <62111876+ZAHID-8017@users.noreply.github.com>
2020-05-20 00:47:17 +0530

    Update Zahid_ass3_10.py

--- Zahid_ass3_10.py
+++ Zahid_ass3_10.py
@@ -5,21 +5,21 @@ import matplotlib.pyplot as plt
 
 
 
-text_file = np.loadtxt(fname = "noise.txt")
+text_file = np.loadtxt(fname = "noise.txt", dtype = float)
 
 
-x = np.linspace(0,512,512)
+#x = np.linspace(0,512,512)
 #print(x)
 #print(text_file)
 
-ft = abs(np.fft.fft(text_file,norm = "ortho"))
-k = np.fft.fftfreq(512,0.001)
+ft = np.fft.fft(text_file, norm = "ortho")
+k = np.fft.fftfreq(512, d=1)
 order = np.argsort(k)
 k = k[order]
-ft =  ft[order]
+aft =  abs(ft[order])
 ################ "PLoting the DFT file" ###############################################
 plt.figure()
-plt.plot(k,ft, "red", label = "DFT_data")
+plt.plot(k,aft, "red", label = "DFT_data")
 plt.grid(True)
 plt.xlabel('frequency')
 plt.ylabel('DFT_data')
@@ -27,7 +27,7 @@ plt.legend()
 ################ "PLoting the text file" ###############################################
 #plt.show()
 fig = plt.figure()
-plt.plot(x,text_file, "green", label = "text_file")
+plt.plot(text_file, "green", label = "text_file")
 plt.grid(True)
 plt.xlabel('index')
 plt.ylabel('file_data')
@@ -36,6 +36,7 @@ plt.legend()
 
 
 spectra=abs(ft)*abs(ft)/(512)
+
 ################ "PLoting the power spectrum" ###############################################
 fig = plt.figure()
 plt.plot(k,spectra, label='Spectra')
@@ -45,8 +46,9 @@ plt.legend()
 
 
 ################ "PLoting the Histogram"###############################################
+bin=10
 fig = plt.figure()
-plt.hist(spectra,10, facecolor='blue',label='binned power spectrum')
+plt.hist(spectra,bin, facecolor='blue',label='binned power spectrum')
 plt.legend()
 plt.show()
 
@@ -60,4 +62,3 @@ plt.show()
 
 
 
-#x += np.random.normal(scale=np.sqrt(noise_power), size=time.shape)
